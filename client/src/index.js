9ddfe9f2d45406feed6a5b3fec116d9baf67f38d
sghwan <anan123456@naver.com>
2021-03-23 21:30:03 +0900

    complete lecture

--- client/src/index.js
+++ client/src/index.js
@@ -15,8 +15,8 @@ const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)
 ReactDOM.render(
   <Provider
     store={createStoreWithMiddleware(Reducer,
-      window.__REDUX_DEVTOOLS_EXTENSTION__ &&
-      window.__REDUX_DEVTOOLS_EXTENSTION__()
+      window.__REDUX_DEVTOOLS_EXTENSION__ && 
+      window.__REDUX_DEVTOOLS_EXTENSION__()
     )}
   >
     <App />
